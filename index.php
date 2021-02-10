1b98fa55e52c81151018f701679cbdb1fa5ec1cb
Niv Noiman <niv945@gmail.com>
2021-02-10 18:57:42 +0200

    Create new files schema

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/styles.css" type="text/css"/>
+	<title>Live User Dashboard</title>
+</head>
+<body>
+	<header class='header-wrapper'>
+		<div class="inner-header">
+
+		</div>
+	</header>
+	<main role="main">
+
+	</main>
+	<footer class="footer-wrapper" role="contentinfo">
+		<span>Thanks, Niv Noiman</span>
+	</footer>
+</body>
+</html>
