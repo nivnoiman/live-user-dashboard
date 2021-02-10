b957450a18392ae72f5e02e7a1b821825144ddf9
Niv Noiman <niv945@gmail.com>
2021-02-10 19:41:23 +0200

    create login form

--- includes/view/login.php
+++ includes/view/login.php
@@ -1,1 +1,14 @@
-<?php
+<div class="login-warpper">
+	<form method="post" id="login-form">
+		<fieldset>
+			<label for="login-email">
+			<span>Email:</span>
+				<input type="text" name="email" id="login-email" value="" placeholder="test@test.com" />
+			</label>
+			<label for="login-password">
+				<span>Password:</span>
+				<input type="password" name="password" id="login-password" value="" placeholder="123456789" />
+			</label>
+		</fieldset>
+	</form>
+</div>
