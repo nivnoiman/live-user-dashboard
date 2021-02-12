274c2d2ae6d7bdafd4b8df37fdb029fbc934b1fe
Niv Noiman <niv945@gmail.com>
2021-02-12 15:06:02 +0200

    create app string for extend languages

---
+++ assets/js/strings.js
@@ -0,0 +1,20 @@
+export const strings = {
+	dashboard: {
+		welcome: 'Welcome %s',
+		not_found: 'No online users where found',
+		username: 'Username:',
+		logintime: 'Login Time:',
+		lastupdate: 'Last Update:',
+		ip: 'Client IP:',
+		agent: 'User-Agent:',
+		createdtime: 'Register time:',
+		logincount: 'Logins count:',
+	},
+	login: {
+		email: 'Email:',
+		password: 'Password:',
+		email_placeholder: 'test%40test.com', // %40 for @
+		password_placeholder: '123456789',
+		btn: 'Login'
+	}
+}
